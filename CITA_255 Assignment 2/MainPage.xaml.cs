43bbd0e04c2b92d406f9d788acf38fc8d4dd51f0
drMEathead225 <hollow269@morrisville.edu>
2026-09-22 20:10:07 -0400

    Add project files.

---
+++ CITA_255 Assignment 2/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.UI.Xaml.Input;
+using Windows.UI.Notifications;
+
+namespace CITA_255_Assignment_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            TaskCollection.ItemsSource = tasks;
+            AmountOfHours.ItemsSource = hours;
+        }
+        List<string> tasks = new List<string>
+        {
+            "Go to class", "Do homework","Go to football practice","Go to the gym","Go to sleep","Eat dinner","Play Videgames","Pray","Watch Film","Do laundry"
+        };
+
+        List<int> hours = new List<int>
+        {
+            2, 3, 4, 1, 8, 2, 1, 1, 2, 1
+        };
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+          double totalHours = 0;
+            foreach (double hour in hours)
+            {
+                totalHours += hour;
+            }
+            TotalHours.Text = $"Total Hours: {totalHours}";
+        }
+    }
+}
